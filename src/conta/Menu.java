918e1db24795ccf3b6d2d8a2d8a837de50b261c1
Desiree Ferreira <desireeferreira6@gmail.com>
2025-02-20 12:08:06 -0300

    Iniciando Menu do Delivery

---
+++ src/conta/Menu.java
@@ -0,0 +1,35 @@
+package conta;
+
+import java.util.Scanner;
+
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+		
+		 int opcao;
+		 
+		 while (true) {
+
+	            System.out.println("*****************************************************");
+	            System.out.println("                                                     ");
+	            System.out.println("          Menu TODO SABOR COMIDA BRASILEIRA          ");
+	            System.out.println("                                                     ");
+	            System.out.println("*****************************************************");
+	            System.out.println("                                                     ");
+	            System.out.println("            1 - Consultar o Cardápio                 ");
+	            System.out.println("            2 - Adicionar item ao Pedido             ");
+	            System.out.println("            3 - Ver Pedido                           ");
+	            System.out.println("            4 - Editar o Pedido                      ");
+	            System.out.println("            5 - Remover o Ite                        ");
+	            System.out.println("            6 - Finalizar o Item                     ");	        
+	            System.out.println("*****************************************************");
+	            System.out.println("Entre com a opção desejada:                          ");
+	            System.out.println("                                                     ");                                          
+		 
+	            opcao = leia.nextInt();
+	}}}
+
+
